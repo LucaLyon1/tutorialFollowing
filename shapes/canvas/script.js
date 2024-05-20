a6e2bab82d3d07c06e729861c003ecd94e3aaeda
Luca Savio <savioluca2@gmail.com>
2024-05-20 12:23:51 +0200

    randomize color :xx

--- shapes/canvas/script.js
+++ shapes/canvas/script.js
@@ -1,5 +1,5 @@
 let balls = [{
-    rad: 10,
+    rad: 20,
     color: "#7F1734",
     x: 50,
     y: 50,
@@ -7,7 +7,7 @@ let balls = [{
     dy: 4
 },
 {
-    rad: 20,
+    rad: 30,
     color: "#7FFF34",
     x: 100,
     y: 100,
@@ -18,12 +18,12 @@ let balls = [{
     rad: 40,
     color: "#2FFF55",
     x: 400,
-    y: 500,
+    y: 200,
     dx: 2,
     dy: 6
 },
 {
-    rad: 5,
+    rad: 50,
     color: "#FF55FF",
     x: 100,
     y: 100,
@@ -50,9 +50,11 @@ function animate() {
         drawBall(b.x, b.y, b.rad, b.color);
         if (b.x + b.dx > canvas.width - b.rad || b.x + b.dx < b.rad) {
             b.dx = -b.dx;
+            b.color = randomizeColor();
         }
         if (b.y + b.dy > canvas.height - b.rad || b.y + b.dy < b.rad) {
             b.dy = -b.dy;
+            b.color = randomizeColor();
         }
         b.dy += gravity;
         b.x += b.dx;
@@ -64,3 +66,13 @@ function start() {
     const interval = setInterval(animate, 16);
 }
 start();
+
+function randomizeColor() {
+    const val = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hex = [];
+    for (i = 0; i < 6; i++) {
+        const rnd = Math.floor(Math.random() * 15);
+        hex.push(val[rnd]);
+    }
+    return '#' + hex.join('');
+}
